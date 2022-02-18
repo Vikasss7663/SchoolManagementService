5bb0e5c085667d7f02850f2b0cbc2e52f250a87c
Vishal Singh <vishalsingh@197NODMBT2926.local>
2022-02-18 20:58:45 +0530

    Course Repository Added

--- src/main/java/com/example/demo/Course.java
+++ src/main/java/com/example/demo/Course.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
 
+    @Id
     private int courseId;
     private String courseName;
 
--- src/main/java/com/example/demo/CourseService.java
+++ src/main/java/com/example/demo/CourseService.java
@@ -2,48 +2,36 @@ package com.example.demo;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
+
+    @Autowired
+    private CourseRepository repository;
     
-    List<Course> courses = new ArrayList<>(
-        Arrays.asList(
-            new Course(1, "C Programming"),
-            new Course(2, "Java Programming"),
-            new Course(3, "Python Programming")
-        )
-    );
+    List<Course> courses = new ArrayList<>();
 
     public List<Course> getCourses() {
+        repository.findAll().forEach(courses::add);
         return courses;
     }
 
     public Course getCourse(int id) {
-        return courses.stream().filter(t -> t.getCourseId() == id)
-                        .findFirst().get();
+        return repository.findById(id).get();
     }
 
     public void addCourse(Course course) {
-        courses.add(course);
+        repository.save(course);
     }
 
     public void updateCourse(int id, Course course) {
-        for(int i=0;i<courses.size();i++) {
-            if(courses.get(i).getCourseId() == id) {
-                courses.set(i, course);
-                return;
-            }
-        }
+        repository.save(course);
     }
 
     public void deleteCourse(int id) {
-        for(int i=0;i<courses.size();i++) {
-            if(courses.get(i).getCourseId() == id) {
-                courses.remove(i);
-                return;
-            }
-        }
+        repository.deleteById(id);
     }
 
 }
