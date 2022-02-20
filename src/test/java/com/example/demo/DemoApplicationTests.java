e13eda9911627de6a91d1d4881971a0caa49e658
Vishal Singh <vishalsingh@197NODMBT2926.local>
2022-02-20 16:00:17 +0530

    MVC Test Added

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+
+		this.mockMvc.perform(get("/welcome")).andDo(print());
+
+		Course fakeCourse = new Course(1, "Updated New Programming");
+
+		this.mockMvc.perform(post("/")
+		.contentType("application/json")
+		.content(new ObjectMapper().writeValueAsString(fakeCourse))
+		);
+
+		this.mockMvc.perform(put("/")
+		.contentType("application/json")
+		.content(new ObjectMapper().writeValueAsString(fakeCourse))
+		);
+
+		this.mockMvc.perform(get("/"))
+		.andDo(print());
+
 	}
 
 }
