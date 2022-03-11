bf45448a8de83dc78fa06821a46b36b659a9fd10
vishalsingh <2018uec1001@gmail.com>
2022-03-11 10:34:53 +0530

    SBT-2 docker-compose resolved
    docker-compose app port issue resolved.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-java -XX:+UseG1GC -Djava.security.egd=file:/dev/./urandom -Dloader.main=com.springboot.tutorial.MainApplication.java -jar app.jar
+java -XX:+UseG1GC  -jar app.jar
 
+# -Djava.security.egd=file:/dev/./urandom -Dloader.main=com.springboot.tutorial.MainApplication.java
 #-Dspring.profiles.active=local
